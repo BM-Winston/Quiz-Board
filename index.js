b40e0d2597cd61ebd42dff0b76481d35fd30c664
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-02-06 11:41:09 +0300

    This commit adds back ground color to document

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 function check() {
-      var c=0;
-      var q-a=document.quiz-board.question-a.value;
-      var q-b=document.quiz-board.question-b.value;
-      var q-c=document.quiz-board.question-c.value;
-      var q-d=document.quiz-board.question-d.value;
-      var q-e=document.quiz-board.question-e.value;
-
-      if (q-a=="Any group of keyboard characters sorrounded by single quotes or double quotes.") {c++}
-      if (q-b=="Values that can either be true or false.") {c++}
-      if (q-c=="False.") {c++}
-      if (q-d=="False") {c++}
-      if (q-e=="All of the above") {c++}
-
-      document.write (c);
-
+    index.write(done);
 }
+    
