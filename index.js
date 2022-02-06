99369b5d4f4a58289226a58d670a8d7657f26a2d
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-02-06 10:07:06 +0300

    This commit adds if statements on the js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function check() {
       var c=0;
-      var qa=document.quiz-board.question-a.value;
-      var qb=document.quiz-board.question-b.value;
-      var qc=document.quiz-board.question-c.value;
-      var qd=document.quiz-board.question-d.value;
-      var qe=document.quiz-board.question-e.value;
+      var q-a=document.quiz-board.question-a.value;
+      var q-b=document.quiz-board.question-b.value;
+      var q-c=document.quiz-board.question-c.value;
+      var q-d=document.quiz-board.question-d.value;
+      var q-e=document.quiz-board.question-e.value;
 
-      if (qa=a"Any group of keyboard characters sorrounded by single quotes or double quotes") {c++}
+      if (q-a=="Any group of keyboard characters sorrounded by single quotes or double quotes.") {c++}
+      if (q-b=="Values that can either be true or false.") {c++}
+      if (q-c=="False.") {c++}
+      if (q-d=="False") {c++}
+      if (q-e=="All of the above") {c++}
+
       document.write (c);
 
 }
