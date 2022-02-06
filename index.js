c94e4a41505924fc388392859223f0822fa681ca
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-02-06 09:56:18 +0300

    This commit adds js function to display result on submission

---
+++ index.js
@@ -0,0 +1,12 @@
+function check() {
+      var c=0;
+      var qa=document.quiz-board.question-a.value;
+      var qb=document.quiz-board.question-b.value;
+      var qc=document.quiz-board.question-c.value;
+      var qd=document.quiz-board.question-d.value;
+      var qe=document.quiz-board.question-e.value;
+
+      if (qa=a"Any group of keyboard characters sorrounded by single quotes or double quotes") {c++}
+      document.write (c);
+
+}
