6328c66c212fbfb822e4c1ef1d1b1ee79da151b5
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-02-09 15:09:01 +0300

    This commit adds js functions2

--- js/index.js
+++ js/index.js
@@ -5,10 +5,16 @@ form.addEventListener('submit', handleForm);
 function checkAnswers(){
    var answer = document.querySelector('input[name="question-a"]:checked').value;
    var answer2 = document.querySelector('input[name="question-b"]:checked').value;
+   var answer3 = document.querySelector('input[name="question-c"]:checked').value;
+   var answer4= document.querySelector('input[name="question-d"]:checked').value;
+   var answer5 = document.querySelector('input[name="question-e"]:checked').value;
+
 
    var grade = 0
    var answerArray = [
-      "abc",
+      "correct5",
+      "correct4",
+      "correct3",
       "correct2",
       "correct",
    ]
@@ -17,6 +23,12 @@ function checkAnswers(){
    if (answerArray[i] === answer) {
       grade += 10;
    }if(answerArray[i] === answer2){
+      grade += 10;
+   }if(answerArray[i] === answer3) {
+      grade += 10;
+   }if(answerArray[i] === answer4) {
+      grade += 10;
+   }if(answerArray[i] === answer5) {
       grade += 10;
    }
    else{
